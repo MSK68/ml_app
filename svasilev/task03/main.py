a27dda5c94f6689660b1b74b5eeaad43b8e5d3f0
Sergei Vasilev <sega.vasiev@gmail.com>
2023-11-24 10:44:42 +1100

    Первая итерация задания 3. Выгрузил тестовый main.py файл

--- svasilev/task03/main.py
+++ svasilev/task03/main.py
@@ -1,21 +1,59 @@
-# sudo fuser -k 8000/tcp
-# Ctrl+C
+"""Приложение создает веб-приложение на основе FastAPI, которое принимает пост запросы с изображением
+и возвращает текстовое описание изображения. Для распознавания изображения используется модель,
+использованная для выполнения задания 2 в папке svasilev/task02/main.py"""
 
 
-from fastapi import FastAPI
-from transformers import pipeline
+import io
+import os
+import uvicorn
+from PIL import Image
 from pydantic import BaseModel
+from typing import Optional
+from fastapi import FastAPI, File, UploadFile
+from transformers import pipeline
 
-class Item(BaseModel):
-    text: str
-
+# Создаем экземпляр класса FastAPI
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+# Создаем класс для хранения данных о изображении
+class ImageData(BaseModel):
+    image: Optional[bytes] = None
 
-@app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text )[0]
+# Создаем экземпляр класса pipeline для распознавания изображения
+pipe = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+
+# Создаем функцию для загрузки изображения
+def load_image(image: ImageData) -> Image.Image:
+    # Получаем изображение
+    image_data = image.image
+    # Возвращаем изображение в формате PIL
+    return Image.open(io.BytesIO(image_data))
+
+# Создаем функцию для распознавания изображения
+def recognize_image(image: Image.Image) -> str:
+    # Запускаем распознавание изображения средствами модели
+    x = pipe(image)
+    # Возвращаем результаты распознавания
+    return x[0]['generated_text']
+
+# Создаем функцию для обработки пост запросов
+@app.post("/predict")
+async def predict(image: ImageData):
+    # Загружаем изображение
+    img = load_image(image)
+    # Распознаем изображение
+    result = recognize_image(img)
+    # Возвращаем результаты распознавания
+    return {"result": result}
+
+# Запускаем сервер
+if __name__ == "__main__":
+    uvicorn.run(app, host="https://krapiva.space", port=8000)
+
+# Запускаем сервер с помощью команды uvicorn main:app --reload
+# Переходим по адресу http://krapiva.space:8000/docs
+# Переходим в раздел POST /predict
+# Нажимаем кнопку Try it out
+# Выбираем изображение для загрузки
+# Нажимаем кнопку Execute
+# Получаем результаты распознавания изображения
