9b903c3004147822a8d8b26e8a0a5ad2d29094b8
segavasilev <sega.vasilev@gmail.com>
2023-12-13 03:10:26 +0000

    Test CI

--- svasilev/task05/test_main.py
+++ svasilev/task05/test_main.py
@@ -8,3 +8,11 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_predict_custom_img():
+    response = client.post("/predict/",
+                           json={"image_url": "https://huggingface.co/datasets/mishig/sample_images/resolve/main/savanna.jpg"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert response.json() == {"result": "a herd of giraffes and zebras grazing in a field "}
