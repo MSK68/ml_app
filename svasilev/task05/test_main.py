f312464d6b595fa9a5b7e3cbbe5822bd03515e8e
Sergei Vasilev <sega.vasiev@gmail.com>
2023-12-13 14:16:15 +1100

    svasilev Task05

--- svasilev/task05/test_main.py
+++ svasilev/task05/test_main.py
@@ -4,15 +4,36 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_main() -> None:
+    """
+    Test the main endpoint
+    :return: None
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
-def test_predict_custom_img():
+def test_predict_custom_img() -> None:
+    """
+    Test the predict endpoint with a custom image
+    :return: None
+    """
     response = client.post("/predict/",
                            json={"image_url": "https://huggingface.co/datasets/mishig/sample_images/resolve/main/savanna.jpg"})
     json_data = response.json()
     assert response.status_code == 200
     assert response.json() == {"result": "a herd of giraffes and zebras grazing in a field "}
+
+
+def test_response_length() -> None:
+    """
+    Test the length of the response
+    :return: None
+    """
+    response = client.post("/predict/",
+                           json={
+                               "image_url": "https://huggingface.co/datasets/mishig/sample_images/resolve/main/savanna.jpg"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert len(json_data["result"]) > 0
